dcd4a13ea66d01386bf64473b4cd0f6134ffdeeb
Thevid Oswald Berrius <thevidob@gmail.com>
2024-05-23 14:56:07 +0700

    test API

--- src/pages/ApiTestPage.jsx
+++ src/pages/ApiTestPage.jsx
@@ -1,18 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Container, Row, Col } from "react-bootstrap";
 import { useBackend } from "../data/useBackend";
+import { useEffect, useState } from "react";
 
 const ApiTestPage = () => {
-    const { InsertCoachOrder } = useBackend();
-    return (
-        <div>
-            <button className="test-api" onClick={
+    const [userData, setUserData] = useState([]);
+    const { GetAllCoaches } = useBackend();
 
-                async () => {
-                    console.log(await InsertCoachOrder())
-                }
-            }>
-                Test API Now
-            </button>
-        </div >
+    useEffect(() => {
+         GetAllCoaches().then(x => setUserData(x.data))
+    }, []);
+
+    return (
+      <div className="api-test">
+        <div>
+          <Container>
+            <Row>
+                {userData.map((coachs) => {
+                    return(
+                    <Col key={coachs.coachID} className="shadow rounded" data-aos="fade-up" data-aos-duration="1000">
+                        <img src={coachs.coachPicture} alt="unsplash.com" className="w-100 mb-5 rounded-top"/>
+                    </Col>
+                    );
+                })}
+            </Row>
+          </Container>
+        </div>
+      </div>
     );
 }
 
